f5c498c9fb75a65a1f566e60801893d90c2c7166
Dracomali <firstgcoders@gmail.com>
2020-09-26 14:57:39 -0400

    new file:   .idea/vcs.xml 	modified:   main.py

--- main.py
+++ main.py
@@ -13,4 +13,24 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv (r'/home/l3phant/Downloads/Churn_Modelling.csv')
 
+sns.set_theme(style="darkgrid")
+
+print(df.shape)
+print(df.columns.values)
 print(df.describe())
+
+print(df['Exited'].value_counts())
+
+
+retained = df[df.Exited == 0]
+churned = df[df.Exited == 1]
+num_retained = retained.shape[0]
+num_churned = churned.shape[0]
+
+print(num_retained/(num_retained + num_churned) * 100, "% of customers stayed with the company.")
+print("")
+print(num_churned / (num_retained + num_churned) * 100,"% of customers left the company.")
+
+
+print(sns.lmplot(x="Tenure", y="Age", hue="Exited", data=df))
+plt.show()
